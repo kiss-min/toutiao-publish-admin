70d9829a66c7a4432dcc7501b7797ed74394e1b5
test_user <2990548316@qq.com>
2021-02-03 20:41:16 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//加载全局样式
+import "./style/index.less"
+
 Vue.config.productionTip = false
 
+//创建vue根实例
+//把router配置到根实例中
 new Vue({
   router,
   store,
   render: h => h(App)
+  //el:"#app"//等价于$mount("#app")
 }).$mount('#app')
